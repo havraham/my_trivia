ea5d45d8c7aabcad0865acf301959253283c78b8
havraham <havraham@gmail.com>
2020-11-11 13:20:25 +0200

    first commit

---
+++ my_trivia/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from my_trivia.users.models import User
+from my_trivia.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
